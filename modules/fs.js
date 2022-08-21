09d803fbaafb1d15627ce7c6e51a4ca8fd4dec4d
Taris Henrique <taris.tj@hotmail.com>
2022-08-20 21:46:23 -0300

    39m32

--- modules/fs.js
+++ modules/fs.js
@@ -2,4 +2,24 @@ const fs = require("fs");
 const path = require("path");
 
 // Criar uma pasta
-fs.mkdir(path.join(__dirname, "/test"), error);
+
+fs.mkdir(path.join(__dirname, "/test"), (error) => {
+  if (error) {
+    return console.log("Erro: ", error);
+  }
+
+  console.log("Pasta criada com sucesso!");
+});
+
+//Criar arquivo
+fs.writeFile(
+  path.join(__dirname, "/test", "teste.txt"),
+  "Hello node!",
+  (error) => {
+    if (error) {
+      return console.log("Erro: ", error);
+    }
+
+    console.log("Arquivo criado com sucesso!");
+  }
+);
