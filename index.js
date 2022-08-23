cc52975726bf404e5062a20e14804b1b1f25d9fc
Taris Henrique <taris.tj@hotmail.com>
2022-08-23 17:32:24 -0300

    57m10 - criar servidor http

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { Person } = require("./person");
 
-require("./modules/path");
-require("./modules/fs");
+//require("./modules/path");
+//require("./modules/fs");
+//require("./modules/http");
+require("./modules/express");
 
 const person = new Person("Taris");
 
-console.log(person.sayMyName());
-console.log(person.sayMyName2());
+//console.log(person.sayMyName());
+//console.log(person.sayMyName2());
---
+++ modules/express.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/home", (req, res) => {
+  res.contentType("application/html");
+  res.status(200).send("<h1> Hello World! </h1>");
+});
+
+app.get("/users", (req, res) => {
+  const users = [
+    {
+      name: "Taris Henrique",
+      email: "taris@trhinfo.com",
+    },
+    {
+      name: "Raabe Castro",
+      email: "raabe@trhinfo.com",
+    },
+  ];
+
+  res.status(200).json(users);
+});
+
+const port = 8080;
+
+app.listen(port, () => console.log(`Rodando com Express na porta ${port}!`));
