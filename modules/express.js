294f2b7281e3ef1d8fb7e5ca6332e0d222bbdfa8
Taris Henrique <taris.tj@hotmail.com>
2022-08-24 11:41:41 -0300

    1h19m20

--- modules/express.js
+++ modules/express.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const UserModel = require("../src/models/user.model");
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.contentType("application/html");
@@ -12,19 +15,22 @@ app.get("/inicio", (req, res) => {
   res.status(200).send("<h1> HomePage </h1>");
 });
 
-app.get("/users", (req, res) => {
-  const users = [
-    {
-      name: "Taris Henrique",
-      email: "taris@trhinfo.com",
-    },
-    {
-      name: "Raabe Castro",
-      email: "raabe@trhinfo.com",
-    },
-  ];
+app.get("/users", async (req, res) => {
+  try {
+    const users = await UserModel.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
 
-  res.status(200).json(users);
+app.post("/users", async (req, res) => {
+  try {
+    const user = await UserModel.create(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 const port = 8080;
